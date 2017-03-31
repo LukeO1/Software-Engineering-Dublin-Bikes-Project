ac2f3d8ebf51bf4b22a3faa4e8e6bfdfcddff508
lyycindy <cindy.lam@ucdconnect.ie>
2017-03-31 12:32:40 +0100

    Successfully established a connection with the database with some simple data returned from the databse. But without the intervention of flask. Now the next step in to incoorperate flask and render this information onto the webapp

--- DB_connection_app.py
+++ DB_connection_app.py
@@ -1,22 +1,9 @@
+"""
 from flask import *
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
 
 app = Flask(__name__)  #creating the App
-
-mysql = SQLAlchemy()
-app.config['MYSQL_DATABASE_USER'] = 'dublinbikes'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'dublinbikes'
-app.config['MYSQL_DATABASE_DB'] = 'dublinbikes'
-app.config['MYSQL_DATABASE_HOST'] = 'dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes'
-mysql.init_app(app)
-#app.config['SQLALCHEMY_DATABASE_URI']='mysql://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes'
-#db =SQLAlchemy(app)
-
-#conn = db.connect()
-#cursor =conn.cursor()
-
-#cursor.callproc('showten')
-
 @app.route('/')  #defining a basic route
 
 def main():
@@ -24,3 +11,15 @@ def main():
 
 if __name__ == "__main__":
     app.run()
+
+"""
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes')
+connection = engine.connect()
+result = connection.execute("select * from dublinbikes.new_bike_stations_real_time limit 10")
+
+for row in result:
+    print(row)
+
+connection.close()
