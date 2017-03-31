b3186ef1bbdbc458b4ad67925d578de96b601c13
lyycindy <cindy.lam@ucdconnect.ie>
2017-03-31 13:55:40 +0100

    Updating with some new functions in the DB connection testing file

--- DB_connection_app.py
+++ DB_connection_app.py
@@ -14,12 +14,16 @@ if __name__ == "__main__":
 
 """
 from sqlalchemy import create_engine
+from flask import Flask, g
+def get_db():
+    if engine not in g or g.engine is None:
+        engine = create_engine('mysql://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes')
 
-engine = create_engine('mysql://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes')
 connection = engine.connect()
 result = connection.execute("select * from dublinbikes.new_bike_stations_real_time limit 10")
 
-for row in result:
-    print(row)
+
+#for row in result:
+#    print(row)
 
 connection.close()
