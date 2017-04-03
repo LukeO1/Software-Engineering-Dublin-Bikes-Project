52c457757e5e71d20aa9fcef1405684542ab5c25
lyycindy <cindy.lam@ucdconnect.ie>
2017-04-03 13:57:22 +0100

    Updating files

--- DB_connection_app.py
+++ DB_connection_app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, g, render_template, jsonify
 from sqlalchemy import create_engine
 
@@ -7,12 +6,12 @@ app = Flask(__name__)  #creating the App
 
 def get_db():
     engine = getattr(g,'engine', None)
-    if engine is None:
+    if engine is None: #If conenction does not exist, create the connection
         engine = create_engine('mysql+mysqldb://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes')
         g.engine = engine
     return engine
 
-#result = connection.execute("select * from dublinbikes.new_bike_stations_real_time limit 10")
+
 @app.route("/station/<int:station_id>")
 def get_station(station_id):
     engine = get_db()
@@ -25,12 +24,13 @@ def get_station(station_id):
     #return "this is station {} {}".format(station_id, engine)
     print(res)
     return jsonify([list(x) for x in res])
+
+
 @app.route('/')  #defining a basic route
 def main():
-    #return render_template('index.html')
-    return "Hello"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
 
 
