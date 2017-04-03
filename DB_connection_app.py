04d05c734da0c192502b98f1115d43271fa40708
Nikki Wong <nikki.wong@ucdconnect.ie>
2017-04-03 22:16:07 +0100

    updating files

--- DB_connection_app.py
+++ DB_connection_app.py
@@ -3,34 +3,36 @@ from flask import Flask, g, render_template, jsonify
 from sqlalchemy import create_engine
 
 
+
 app = Flask(__name__)  #creating the App
 
 def get_db():
     engine = getattr(g,'engine', None)
     if engine is None:
-        engine = create_engine('mysql+mysqldb://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes')
+        engine = create_engine('mysql+pymysql://dublinbikes:dublinbikes@dublinbikes.ce0mzpzqjeei.us-west-2.rds.amazonaws.com/dublinbikes')
         g.engine = engine
     return engine
 
 #result = connection.execute("select * from dublinbikes.new_bike_stations_real_time limit 10")
-@app.route("/station/<int:station_id>")
-def get_station(station_id):
+@app.route("/station")
+def get_station():
     engine = get_db()
+
     sql = """
-    select name, number
-    from dublinbikes.new_bike_stations_real_time 
+    select *
+    from dublinbikes.new_bike_stations_real_time
     limit 10;
     """
     res = engine.execute(sql).fetchall()
-    #return "this is station {} {}".format(station_id, engine)
+    # return "this is station {} {}".format(station_id, engine)
     print(res)
-    return jsonify([list(x) for x in res])
+    return jsonify([dict(row.items()) for row in res])
 @app.route('/')  #defining a basic route
 def main():
-    #return render_template('index.html')
-    return "Hello"
+    return render_template('DBikes.html')
+    # return "Hello"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
 
 
