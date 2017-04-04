6fa3bef097b42a1c91e72398473c87e38d296bf9
lyycindy <cindy.lam@ucdconnect.ie>
2017-04-04 01:12:31 +0100

    Backing up

--- static/J_test.js
+++ static/J_test.js
@@ -1,11 +1,8 @@
 function myMap() {
 
-}
-    var myOptions = {
+    var focus = {lat: 53.343793, lng: -6.254572}
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
-        center: {lat: 53.343793, lng: -6.254572},
-        mapTypeControl: false
-    }
-
-    map = new google.maps.Map(document.getElementById("Gmap"), myOptions
-    );
+        center: focus
+    });
+}
