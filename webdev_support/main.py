af28ec74788527c86b643cbeb6f900a56595ed29
LukeO1 <londerwater1@gmail.com>
2017-04-05 22:05:03 +0100

    minor changes to update

--- webdev_support/main.py
+++ webdev_support/main.py
@@ -1,5 +1,7 @@
 from flask import Flask, g, jsonify, render_template
 from sqlalchemy import create_engine
+import pandas
+import numpy
 
 app = Flask(__name__, static_url_path='')
 
@@ -10,6 +12,22 @@ def get_db():
         engine = create_engine('mysql+pymysql://dublinbikes:dublinbikes@dublinbikes.c2xnnykekrgc.us-west-2.rds.amazonaws.com/dublinbikes')
         g.engine = engine
     return engine
+
+#@app.route('/detailed/<int:station_id>')
+@app.route('/detailed/<string:station_name>')
+def get_occupancy(station_name):
+    engine = get_db()
+    #number = engine.execute("SELECT number from static_information where")
+    df = pandas.read_sql_query("SELECT available_bike_stands, available_bikes, last_update from station_info where name=%(name)s and status='OPEN' order by last_update DESC", engine, params={'name':station_name})
+    df.set_index('last_update', inplace=True)
+    #df.index.name = None
+    df = df.resample('D')
+    df = df.reset_index()
+    df['day_of_week'] = df['last_update'].dt.weekday_name
+    #df['last_update'] = df.index
+    x = df.to_html(classes='station')
+    return x
+    #return render_template('index.html', data=data)
 
 @app.route('/')
 def main():
